21cb3712990b41167a48adf064f9f2aea89bbcc7
roy <roy.cruz@upr.edu>
2022-09-22 16:52:09 -0400

    Code works for n = 1

--- HW02/ring.c
+++ HW02/ring.c
@@ -38,52 +38,35 @@
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
-
     int comm_sz, my_rank;
+    
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    char my_msg[1000], recvd_msg[1000];
-    int msg_size = 0;
+    char my_msg[100], msg[100];
     sprintf(my_msg, "Rank%d", my_rank);
 
-    if ((my_rank == 0) && comm_sz > 1) {
-        printf(my_msg);
-        MPI_Send(my_msg, strlen(my_msg), MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-    } else {
-        printf(my_msg);
-        printf("\n");
-        printf("Process done!");
-        return 0;
+    if (my_rank == 0) { // Rank 0 initializes message to be sent around
+        sprintf(msg, "%s %s", msg, my_msg);
+    } else { // Other processes wait to recieve message from process my_rank-1. After recieving, they append their contribution to the message
+        MPI_Recv(msg, 100, MPI_CHAR, my_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%s \n", msg);
+        sprintf(msg, "%s %s", msg, my_msg);
     }
 
-    for (int i = 1; i <= comm_sz; i++) 
-        if (i == my_rank) {
-            strcat(local_msg, " ");
-            MPI_Recv(recvd_msg, 1000, MPI_CHAR, i-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            strcat(local_msg, recvd_msg);
-            MPI_Send(local_msg, 1000, MPI_CHAR, i + 1)
-        } else {
-            continue;
-        }
+    // Chain sending/recieving starts.
+    if (my_rank < comm_sz-1) {
+        MPI_Send(msg, 100, MPI_CHAR, my_rank+1, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Send(msg, 100, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
     }
+
+    if (my_rank == 0) {
+        MPI_Recv(msg, 100, MPI_CHAR, comm_sz - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("%s\n", msg);
+        printf("Communication complete!\n");
+    }
+
     MPI_Finalize();
     return 0;
 }
-
-/* We need to modify this file so that we send point-to-point messages
-    * between processes. We'll use MPI_Send and MPI_Recv... */
-
-/* MPI_Send example:
-    * char buffer[1000];
-    * sprintf(buffer, "Hello world from %d\n", rank);
-    * MPI_Send(buffer, 1000, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-    *     Where i is the rank to send to
-    *
-    * MPI_Recv example:
-    * char buffer[1000];
-    * MPI_Recv(buffer, 1000, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    *    Where i is the rank to receive from
-    */
-
-/* Clean up */
